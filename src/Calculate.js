8d39d938fdafe22712b0ed1be566c15d88a478ff
olci34 <developerogulcan@gmail.com>
2021-09-19 22:45:54 -0400

    define #splitByPlusAndCalculate method that handles only addition

--- src/Calculate.js
+++ src/Calculate.js
@@ -5,7 +5,10 @@ function Calculate() {
     const [input, setInput] = useState("")
     const [result, setResult] = useState("")
     const handleChange = (e) => { setInput(e.target.value) }
-    const handleCalculateButton = () => { console.log(input) }
+    const handleCalculateButton = () => { 
+        let result = splitByPlusAndCalculate(input) 
+        setResult(result)
+    }
 
     return (
         <div>
@@ -14,6 +17,13 @@ function Calculate() {
             <h3> Result: {result} </h3>
         </div>
     )
+}
+
+function splitByPlusAndCalculate(str) {
+    const strArr = str.split("+")
+    const intArr = strArr.map(str => parseInt(str))
+    const result = intArr.reduce((acc, no) => acc + no)
+    return result
 }
 
 export default Calculate
